040bf270714beff553765fb12cedb46af41b6425
DineshOSOS <dinesh.ranga@osos.om>
2025-07-29 18:55:59 +0530

    chore(idl): Idl apply btn change [AQAL-4642]

--- src/app/modules/client/idl-apply/idl-apply.component.ts
+++ src/app/modules/client/idl-apply/idl-apply.component.ts
@@ -5,9 +5,11 @@ import {StepperOrientation, MatStepperModule} from '@angular/material/stepper';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {MatButtonModule} from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {AsyncPipe} from '@angular/common';
+import { Router } from '@angular/router';
 import { MaterialModules } from '../../../material';
 import { NavbarComponent } from "../../common/components/navbar/navbar.component";
 import { PrerequisitesComponent } from "./components/prerequisites/prerequisites.component";
@@ -16,6 +18,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { TermsAndConditionComponent } from "./components/terms-and-condition/terms-and-condition.component";
 import { ApprovalsComponent } from "./components/approvals/approvals.component";
 import { FeeAndChargersComponent } from "./components/fee-and-chargers/fee-and-chargers.component";
+import { SignInComponent } from '../../common/sign-in/sign-in.component';
 @Component({
   standalone: true,
   selector: 'app-idl-apply',
@@ -40,6 +43,8 @@ import { FeeAndChargersComponent } from "./components/fee-and-chargers/fee-and-c
 })
 export class IdlApplyComponent {
   private _formBuilder = inject(FormBuilder);
+  private router = inject(Router);
+  private dialog = inject(MatDialog);
 
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
@@ -66,5 +71,17 @@ export class IdlApplyComponent {
 
   ngOnInit() {
     this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
+
+  navigateToLoginPage() {
+    const dialogRef = this.dialog.open(SignInComponent, {
+      height: 'auto',
+      width: '40em',
+      panelClass: 'default-preview-dialog',
+      });
   }
 }
