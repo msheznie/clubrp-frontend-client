2ac83356fd5cc48bf21e8e168ee105d6f058149b
DineshOSOS <dinesh.ranga@osos.om>
2025-07-29 17:38:20 +0530

    feat(idl): Idl pre-requestion [AQAL-4642]

--- src/app/modules/client/idl-apply/components/prerequisites/prerequisites.component.ts
+++ src/app/modules/client/idl-apply/components/prerequisites/prerequisites.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-prerequisites',
   templateUrl: './prerequisites.component.html',
   styleUrls: ['./prerequisites.component.scss'],
   standalone: true,
-  imports: []
+  imports: [CommonModule],
+  encapsulation: ViewEncapsulation.None
 })
-export class PrerequisitesComponent {
+export class PrerequisitesComponent implements OnInit {
+  private api = inject(ApiService);
+  prerequisites: any = {};
 
+  ngOnInit() {
+    this.loadPrerequisites();
+  }
+
+  loadPrerequisites() {
+    this.api.get<any>('/idl/prerequisites').subscribe({
+      next: (response) => {
+        this.prerequisites = response.data;
+      },
+      error: (error) => {
+        console.error('Error loading prerequisites:', error);
+      }
+    });
+  }
 }
